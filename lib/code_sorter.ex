200d9765a431681ec1a3577941a48d8a2904ca44
Álvaro Vilanova Vidal <alvivi@gmail.com>
2020-04-13 19:38:27 +0200

    Bootstrap

---
+++ lib/code_sorter.ex
@@ -0,0 +1,18 @@
+defmodule CodeSorter do
+  @moduledoc """
+  Documentation for `CodeSorter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodeSorter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/code_sorter_test.exs
@@ -0,0 +1,8 @@
+defmodule CodeSorterTest do
+  use ExUnit.Case
+  doctest CodeSorter
+
+  test "greets the world" do
+    assert CodeSorter.hello() == :world
+  end
+end
